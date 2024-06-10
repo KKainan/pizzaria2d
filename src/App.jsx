a83c304e9286f3d2de4ee88fc0c154d8bf34d0c3
KKainan <kainanamajiki@gmail.com>
2024-06-10 11:02:17 -0300

    Pizzaria2d

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Inicio from "./pages/Inicio";
+
 const App = () => {
 
   const [color] = useState("#eb568f")
@@ -9,7 +11,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/inicio" elemrnt={<Inicio />} />
+      <Route path="/inicio" element={<Inicio />} />
     </Routes>
   )
 }
